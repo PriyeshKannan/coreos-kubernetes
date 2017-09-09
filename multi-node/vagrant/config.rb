0f9714f0ac2c33a2a8399ba627924bd8d57f2b7c
Michal Cwienczek <michael.cwienczek@gmail.com>
2017-09-09 18:49:56 +0200

    Added config.rb

---
+++ multi-node/vagrant/config.rb
@@ -0,0 +1,13 @@
+$update_channel="stable"
+
+$controller_count=1
+$controller_vm_memory=1024
+
+$worker_count=3
+$worker_vm_memory=2048
+
+$etcd_count=1
+$etcd_vm_memory=512
+
+$controller_vm_vcpus=1
+$worker_vm_vcpus=1
